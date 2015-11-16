88054ff0808870302b5352b7b83ea123ffbbff25
Prashant Yadav <yadavprashant08@gmail.com>
2015-11-16 18:28:06 +0530

    Adding the proper configuration in the config file. This will make the life easy on the webserver while fetch

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/marriage';
+$config['base_url'] = 'http://www.sakshiprashant.com';
 
 /*
 |--------------------------------------------------------------------------
