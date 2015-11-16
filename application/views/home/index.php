3657bb54b5f656d4b40b5430bf9e9b12ec4490ab
Prashant Yadav <yadavprashant08@gmail.com>
2015-11-16 19:35:20 +0530

    Transparent background

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,187 +1,195 @@
-<!-- Featured -->
-<div class="wrapper style1">
-    <section class="container">
-        <header class="major">
-            <div class="row">
-                <div class="col-md-3">&nbsp;</div>
-                <div class="col-md-1"><i class="fa fa-windows fa-4x"></i></div>
-                <div class="col-md-4"><h2>Bride &amp; Groom</h2>
-                    <!--<light>Sakshi &amp; Prashant</light>-->
-                </div>
-                <div class="col-md-1"><i class="fa fa-amazon fa-4x"></i></div>
-                <div class="col-md-3">&nbsp;</div>
-            </div>
-        </header>
-        <section>
-            <?php
-            $_col_style_class_img = "col-xs-12 col-sm-12 col-md-3 col-lg-3 text-center";
-            $_col_style_class_side = "col-xs-12 col-sm-12 col-md-2 col-lg-2 text-center bride_groom_div";
-            ?>
-            <div class="row">
-                <div class="<?php echo $_col_style_class_side; ?>">&nbsp;</div>
-                <div class="<?php echo $_col_style_class_img; ?>">
-                    <a class="bride_groom_a" href="<?php echo assets_url('images/bride_groom.jpg'); ?>">
-                        <div class="animated bride_groom bride" style="margin-right: 0px;" data-animation="flipInY"
-                             data-animation-delay="100">
-                            &nbsp;</div>
-                    </a>
-                    <!--<i class="fa fa-venus fa-2x"></i>-->
-                </div>
-                <div class="<?php echo $_col_style_class_side; ?>">
-                    <i class="fa fa-heart fa-5x"></i>
-                </div>
-                <div class="<?php echo $_col_style_class_img; ?>">
-                    <a class="bride_groom_a" href="<?php echo assets_url('images/bride_groom.jpg'); ?>">
-                        <div class="animated bride_groom groom" style="margin-left: 0px;" data-animation="flipInY"
-                             data-animation-delay="100">
-                            &nbsp;</div>
-                    </a>
-                    <!--<i class="fa fa-mars fa-2x"></i>-->
-                </div>
-                <div class="<?php echo $_col_style_class_side; ?>">&nbsp;</div>
-                <div class="col-md-12">
-                    &nbsp;
-                </div>
-            </div>
-
-        </section>
-    </section>
-    <div class="image-arrows2">&nbsp;</div>
-</div>
-<!-- Main -->
-<div class="wrapper style2">
-    <section class="container">
-        <header>
-            <h2 class="animated" data-animation="fadeIn" data-animation-delay="100"><span>When</span><span
-                    style="color: #ff0000;">&nbsp;&amp;&nbsp;</span><span class="animate-plus"
-                                                                          data-animations="bounceIn"
-                                                                          data-delay="100">Where</span></h2>
-        </header>
-        <div class="row">
-            <div class=" col-xs-12 col-md-4 col-sm-12 col-lg-4">
-                <h2><i class="fa fa-bell fa-1x"></i> Ladies Sangeet</h2>
-                <h4>Sunday December 06, 2015</h4>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-lg-2 col-md-2">
-                &nbsp;
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 border-map animated" data-animation="zoomIn">
-                <iframe class="border-map" width="100%" frameborder="0" style="border:0"
-                        src="https://www.google.com/maps/embed/v1/place?q=Hotel%20Laxmi%20Villas%2C%20Narayan%20Singh%20Circle%2C%20Jaipur&key=AIzaSyBxyqIPaqi9pCu6owQ2Av8tRld82rcZp2A"
-                        allowfullscreen></iframe>
-            </div>
-
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                &nbsp;
-            </div>
-
-            <div class=" col-xs-12 col-md-4 col-sm-12 col-lg-4">
-                <h2><i class="fa fa-bell fa-1x"></i> Marriage</h2>
-                <h4>Monday December 07, 2015</h4>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-lg-2 col-md-2">
-                &nbsp;
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 border-map fadeIn animated"
-                 data-animation-delay="500" data-animation="zoomIn">
-                <iframe width="100%" frameborder="0" style="border:0"
-                        src="https://www.google.com/maps/embed/v1/place?q=EP%20Entertainment%20Pradise%2C%20Jaipur%2C%20Rajasthan%2C%20India&key=AIzaSyBxyqIPaqi9pCu6owQ2Av8tRld82rcZp2A"
-                        allowfullscreen></iframe>
-            </div>
-        </div>
-    </section>
-    <div class="image-arrows">&nbsp;</div>
-</div>
-
-<div class="wrapper style1">
-    <section class="container">
-        <header class="major">
-            <h2 class="h2-center">Family</h2>
-        </header>
-
-        <div class="row family-tree">
-            <div class="col-sm-12 col-xs-12 col-md-4 col-lg-3 family-intro">
-                <img class="lazyload" src="<?php echo assets_url('images/SatyaveerSingh.jpg') ?>">
-
-                <h3>Dr. Satyaveer Singh</h3>
-            </div>
-
-            <div class="col-sm-12 col-xs-12 col-md-4 col-lg-3 family-intro">
-                <img class="lazyload" src="<?php echo assets_url('images/RajniDevi.jpg') ?>">
-
-                <h3>Smt. Rajni Devi</h3>
-            </div>
-            <div class="bride-family">
-                <div class="col-sm-12 col-xs-12 col-md-4 col-lg-3 family-intro">
-                    <img class="lazyload" src="<?php echo assets_url('images/SDAgarwal.jpg') ?>">
-
-                    <h3>Sh. S.D. Agarwal</h3>
-                </div>
-
-                <div class="col-sm-12 col-xs-12 col-md-4 col-lg-3 family-intro">
-                    <img class="lazyload" src="<?php echo assets_url('images/SarojAgarwal.jpg') ?>">
-
-                    <h3>Smt. Saroj Agarwal</h3>
-                </div>
-            </div>
-        </div>
-    </section>
-    <div class="image-arrows2">&nbsp;</div>
-</div>
-
-<div class="wrapper style2">
-    <section class="container">
-
-    </section>
-    <div class="image-arrows">&nbsp;</div>
-</div>
-
-<?php $gal_images = array('pic01.jpg',
-    'pic02.jpg',
-    'pic03.jpg',
-    'pic04.jpg',
-    'pic05.jpg',
-    'pic06.jpg',
-    'pic07.jpg',
-    'pic08.jpg',
-    'pic09.jpg',
-    'pic10.jpg',
-    'pic11.jpg',
-    'pic12.jpg'); ?>
-
-<!-- Featured -->
-<div class="wrapper style1">
-    <div id="gallery" class="container">
-        <header class="major">
-            <h2 class="animated" data-animation="slideInLeft" data-animation-delay="150">Gallery</h2>
-        </header>
-        <div class="row my-gallery">
-            <?php
-            foreach ($gal_images as $gal_img) {
-                $url = assets_url('images/photo-galery');
-                $img_url = "{$url}/{$gal_img}";
-                ?>
-                <a href="<?php echo $img_url; ?>">
-                    <div class="col-sm-12 col-xs-12 col-md-4 col-lg-3 gallery-img-container">
-                        <img class="left photo-galary-small-img"
-                             src="<?php echo $img_url; ?>"
-                             alt="">
+<div class="page-parallax-bg">
+    <!-- Featured -->
+    <div class="wrapper style1">
+        <section class="container">
+            <header class="major">
+                <div class="row">
+                    <div class="col-md-3">&nbsp;</div>
+                    <div class="col-md-1"><i class="fa fa-windows fa-4x"></i></div>
+                    <div class="col-md-4"><h2>Bride &amp; Groom</h2>
+                        <!--<light>Sakshi &amp; Prashant</light>-->
                     </div>
-                </a>
+                    <div class="col-md-1"><i class="fa fa-amazon fa-4x"></i></div>
+                    <div class="col-md-3">&nbsp;</div>
+                </div>
+            </header>
+            <section>
                 <?php
-            }
-            ?>
-        </div>
+                $_col_style_class_img = "col-xs-12 col-sm-12 col-md-3 col-lg-3 text-center";
+                $_col_style_class_side = "col-xs-12 col-sm-12 col-md-2 col-lg-2 text-center bride_groom_div";
+                ?>
+                <div class="row">
+                    <div class="<?php echo $_col_style_class_side; ?>">&nbsp;</div>
+                    <div class="<?php echo $_col_style_class_img; ?>">
+                        <a class="bride_groom_a" href="<?php echo assets_url('images/bride_groom.jpg'); ?>">
+                            <div class="animated bride_groom bride" style="margin-right: 0px;" data-animation="flipInY"
+                                 data-animation-delay="100">
+                                &nbsp;</div>
+                        </a>
+                        <!--<i class="fa fa-venus fa-2x"></i>-->
+                    </div>
+                    <div class="<?php echo $_col_style_class_side; ?>">
+                        <i class="fa fa-heart fa-5x"></i>
+                    </div>
+                    <div class="<?php echo $_col_style_class_img; ?>">
+                        <a class="bride_groom_a" href="<?php echo assets_url('images/bride_groom.jpg'); ?>">
+                            <div class="animated bride_groom groom" style="margin-left: 0px;" data-animation="flipInY"
+                                 data-animation-delay="100">
+                                &nbsp;</div>
+                        </a>
+                        <!--<i class="fa fa-mars fa-2x"></i>-->
+                    </div>
+                    <div class="<?php echo $_col_style_class_side; ?>">&nbsp;</div>
+                    <div class="col-md-12">
+                        &nbsp;
+                    </div>
+                </div>
+
+            </section>
         </section>
     </div>
     <div class="image-arrows2">&nbsp;</div>
-</div>
+    <!-- Main -->
+    <div class="wrapper style2">
+        <!--<div class="page-parallax-bg">
+            <div class="bg_wrapper2">-->
+        <section class="container">
 
-<div class="wrapper style2">
+            <header>
+                <h2 class="animated" data-animation="fadeIn" data-animation-delay="100"><span>When</span><span
+                        style="color: #ff0000;">&nbsp;&amp;&nbsp;</span><span class="animate-plus"
+                                                                              data-animations="bounceIn"
+                                                                              data-delay="100">Where</span></h2>
+            </header>
+            <div class="row">
+                <div class=" col-xs-12 col-md-4 col-sm-12 col-lg-4">
+                    <h2><i class="fa fa-bell fa-1x"></i> Ladies Sangeet</h2>
+                    <h4>Sunday December 06, 2015</h4>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-lg-2 col-md-2">
+                    &nbsp;
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 border-map animated" data-animation="zoomIn">
+                    <iframe class="border-map" width="100%" frameborder="0" style="border:0"
+                            src="https://www.google.com/maps/embed/v1/place?q=Hotel%20Laxmi%20Villas%2C%20Narayan%20Singh%20Circle%2C%20Jaipur&key=AIzaSyBxyqIPaqi9pCu6owQ2Av8tRld82rcZp2A"
+                            allowfullscreen></iframe>
+                </div>
 
-    <div class="image-arrows">&nbsp;</div>
-</div>
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    &nbsp;
+                </div>
 
-<div class="wrapper style1" style="height: 20vh;">
+                <div class=" col-xs-12 col-md-4 col-sm-12 col-lg-4">
+                    <h2><i class="fa fa-bell fa-1x"></i> Marriage</h2>
+                    <h4>Monday December 07, 2015</h4>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-lg-2 col-md-2">
+                    &nbsp;
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 border-map fadeIn animated"
+                     data-animation-delay="500" data-animation="zoomIn">
+                    <iframe width="100%" frameborder="0" style="border:0"
+                            src="https://www.google.com/maps/embed/v1/place?q=EP%20Entertainment%20Pradise%2C%20Jaipur%2C%20Rajasthan%2C%20India&key=AIzaSyBxyqIPaqi9pCu6owQ2Av8tRld82rcZp2A"
+                            allowfullscreen></iframe>
+                </div>
+            </div>
+
+        </section>
+        <!--</div>
+    </div>-->
+        <div class="image-arrows">&nbsp;</div>
+    </div>
+
+    <div class="wrapper style1">
+        <section class="container">
+            <header class="major">
+                <h2 class="h2-center">Family</h2>
+            </header>
+
+            <div class="row family-tree">
+                <div class="col-sm-12 col-xs-12 col-md-4 col-lg-3 family-intro">
+                    <img class="lazyload" src="<?php echo assets_url('images/SatyaveerSingh.jpg') ?>">
+
+                    <h3>Dr. Satyaveer Singh</h3>
+                </div>
+
+                <div class="col-sm-12 col-xs-12 col-md-4 col-lg-3 family-intro">
+                    <img class="lazyload" src="<?php echo assets_url('images/RajniDevi.jpg') ?>">
+
+                    <h3>Smt. Rajni Devi</h3>
+                </div>
+                <div class="bride-family">
+                    <div class="col-sm-12 col-xs-12 col-md-4 col-lg-3 family-intro">
+                        <img class="lazyload" src="<?php echo assets_url('images/SDAgarwal.jpg') ?>">
+
+                        <h3>Sh. S.D. Agarwal</h3>
+                    </div>
+
+                    <div class="col-sm-12 col-xs-12 col-md-4 col-lg-3 family-intro">
+                        <img class="lazyload" src="<?php echo assets_url('images/SarojAgarwal.jpg') ?>">
+
+                        <h3>Smt. Saroj Agarwal</h3>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+    <div class="image-arrows2">&nbsp;</div>
+    <div class="wrapper style2">
+        <section class="container">
+
+        </section>
+        <div class="image-arrows">&nbsp;</div>
+    </div>
+
+    <?php $gal_images = array('pic01.jpg',
+        'pic02.jpg',
+        'pic03.jpg',
+        'pic04.jpg',
+        'pic05.jpg',
+        'pic06.jpg',
+        'pic07.jpg',
+        'pic08.jpg',
+        'pic09.jpg',
+        'pic10.jpg',
+        'pic11.jpg',
+        'pic12.jpg'); ?>
+
+    <!-- Featured -->
+    <div class="wrapper style1">
+        <div id="gallery" class="container">
+            <header class="major">
+                <h2 class="animated" data-animation="slideInLeft" data-animation-delay="150">Gallery</h2>
+            </header>
+            <div class="row my-gallery">
+                <?php
+                foreach ($gal_images as $gal_img) {
+                    $url = assets_url('images/photo-galery');
+                    $img_url = "{$url}/{$gal_img}";
+                    ?>
+                    <a href="<?php echo $img_url; ?>">
+                        <div class="col-sm-12 col-xs-12 col-md-4 col-lg-3 gallery-img-container">
+                            <img class="left photo-galary-small-img"
+                                 src="<?php echo $img_url; ?>"
+                                 alt="">
+                        </div>
+                    </a>
+                    <?php
+                }
+                ?>
+            </div>
+            </section>
+        </div>
+    </div>
+    <div class="image-arrows2">&nbsp;</div>
+
+    <div class="wrapper style2">
+
+        <div class="image-arrows">&nbsp;</div>
+    </div>
+
+    <div class="wrapper style1" style="height: 20vh;">
+
+    </div>
 
 </div>
